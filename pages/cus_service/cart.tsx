86b8a3001cf521158fff00341cb1dd3c490823d3
DESKTOP-U12NNCF\Minhminle <voleminhnghia176202@gmail.com>
2024-05-11 23:20:06 +0700

    on let app

--- pages/cus_service/cart.tsx
+++ pages/cus_service/cart.tsx
@@ -30,18 +30,18 @@ const Cart = () => {
     measurementId: "G-QSTWZS9F78",
   };
   // // Kiểm tra xem ứng dụng Firebase đã tồn tại chưa
-  // let app;
-  // try {
-  //   app = getApp();
-  // } catch (error) {
-  //   // Ứng dụng Firebase chưa tồn tại, hãy khởi tạo mới
-  //   app = initializeApp(firebaseConfig);
-  // }
+  let app;
+  try {
+    app = getApp();
+  } catch (error) {
+    // Ứng dụng Firebase chưa tồn tại, hãy khởi tạo mới
+    app = initializeApp(firebaseConfig);
+  }
 
-  // // Sử dụng ứng dụng Firebase đã khởi tạo để tạo Firestore
-  // const firestore = getFirestore(app);
-  const app = initializeApp(firebaseConfig);
+  // Sử dụng ứng dụng Firebase đã khởi tạo để tạo Firestore
   const firestore = getFirestore(app);
+  // const app = initializeApp(firebaseConfig);
+  // const firestore = getFirestore(app);
   const router = useRouter();
   const [note, setNote] = useState(""); // State để lưu trữ giá trị ghi chú
   const [products, setProducts] = useState<
